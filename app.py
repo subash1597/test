5e304810348bcf839497973e9a956db2ee42d691
subash1597 <subashbharathkm@gmail.com>
2021-10-22 17:54:39 +0530

    test workflow-1

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be INR {}'.format(output))
+    return render_template('index.html', prediction_text='Employee Salary should be $$$ {}'.format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
